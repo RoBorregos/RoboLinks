b8271a801203d0b707ba5169562f85d719ec3999
Oscar-gg <oscar.arreola.jr@gmail.com>
2024-06-17 20:59:33 -0600

    auth3

--- src/utils/env.mjs
+++ src/utils/env.mjs
@@ -11,8 +11,8 @@ export const env = createEnv({
        GOOGLE_CLIENT_ID: z.string(),
        GOOGLE_CLIENT_SECRET: z.string(),
 
-       GITHUB_CLIENT_ID: z.string(),
-       GITHUB_CLIENT_SECRET: z.string(),
+      //  GITHUB_CLIENT_ID: z.string(),
+      //  GITHUB_CLIENT_SECRET: z.string(),
        // NODE_ENV: z.enum(["development", "test", "production"]),
     // NEXTAUTH_SECRET:
     //   process.env.NODE_ENV === "production"
@@ -58,8 +58,8 @@ export const env = createEnv({
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    // GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    // GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
